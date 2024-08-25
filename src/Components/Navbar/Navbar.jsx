6ecdd2b7e074331c10f46aa6057c5d5d02e976ed
habib ulla <habibulla1278@gmail.com>
2024-08-25 14:38:50 +0600

    second navber added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   FaMapMarkerAlt,
   FaUsers,
@@ -10,12 +10,19 @@ import {
   FaUser,
   FaHeart,
   FaInstagram,
+  FaSearch,
 } from "react-icons/fa";
 import { CiHeart, CiSearch, CiUser } from "react-icons/ci";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { BsBoxSeam } from "react-icons/bs";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <div>
       {/* first section of navbar  */}
@@ -68,56 +75,114 @@ const Navbar = () => {
           </div>
         </div>
       </div>
+
       {/* second  section of navbar  */}
       <div className="">
-        {" "}
-        <div className="max-w-7xl m-auto md:px-10 px-5"></div>
+        <div className="max-w-7xl m-auto md:px-10 px-5 py-[8px]">
+          <div className="grid lg:grid-cols-3 grid-cols-2 w-full ">
+            {/* first section  */}
+            <div className="lg:block hidden">
+              <div className="flex h-12 items-center">
+                <input
+                  type="text"
+                  className="h-full px-3 border-gray-200 text-gray-900 outline-none border-y border-l "
+                  placeholder="Find our seach"
+                />
+                <button className="h-full px-4 text-white flex justify-center items-center text-center bg-[#333333]  hover:bg-[#F14772] text-xl duration-500">
+                  <FaMagnifyingGlass />
+                </button>
+              </div>
+            </div>
+
+            {/* second section  */}
+            <div className="flex lg:justify-center md:justify-start items-center">
+              <Link to={"/"} className="text-3xl text-[#333333]">
+                <span
+                  style={{
+                    background: "linear-gradient(to right, #7957CF, #BE4E9A)",
+                    WebkitBackgroundClip: "text",
+                    WebkitTextFillColor: "transparent",
+                    fontWeight: "900",
+                  }}
+                >
+                  Zen
+                </span>
+                Mart
+              </Link>
+            </div>
+            {/* third section  */}
+            <div className="flex justify-end text-[#333333] ">
+              <div className="flex gap-5 items-center md:text-2xl text-xl">
+                <div className="drawer">
+                  <input
+                    id="my-drawer"
+                    type="checkbox"
+                    className="drawer-toggle"
+                    checked={isOpen}
+                    onChange={toggleDrawer}
+                  />
+                  <div className="drawer-content">
+                    {/* Page content here */}
+
+                    <label
+                      htmlFor="my-drawer"
+                      className=" bg-transparent shadow-none flex items-center justify-center border-none hover:bg-transparent outline-none  drawer-button "
+                    >
+                      <div>
+                        <div className="hover:text-[#F14772] duration-500 md:text-2xl text-xl lg:hidden block">
+                          <CiSearch />
+                        </div>
+                      </div>
+                    </label>
+                  </div>
+                  <div className={`drawer-side ${isOpen ? "drawer-open" : ""}`}>
+                    <label
+                      htmlFor="my-drawer"
+                      aria-label="close sidebar"
+                      className="drawer-overlay"
+                      onClick={toggleDrawer}
+                    ></label>
+                    <div className=" bg-base-200 text-base-content min-w-full h-24 p-4">
+                      {/* Sidebar content here */}
+                      <div className="max-w-7xl m-auto px-10 flex flex-col  justify-end h-full ">
+                        <div className=" relative border-b border-[#F14772]">
+                          <input
+                            type="text"
+                            className="w-full text-base py-1 outline-none bg-transparent "
+                            placeholder="Find our search"
+                          />
+                          <CiSearch className="text-[#F14772] absolute   right-0 top-2" />
+                          <span
+                            onClick={toggleDrawer}
+                            className="text-[#F14772] absolute cursor-pointer   right-2 -top-7"
+                          >
+                            x
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <NavLink className="hover:text-[#F14772] flex items-center  gap-1 duration-500">
+                  <CiHeart />
+                  <span className="text-xl">(0)</span>
+                </NavLink>
+                <NavLink className="hover:text-[#F14772] flex items-center gap-1 duration-500">
+                  <BsBoxSeam /> <span className="text-xl">(0)</span>
+                </NavLink>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
       {/* Third section of navbar  */}
       <div className="">
-        {" "}
         <div className="max-w-7xl m-auto md:px-10 px-5"></div>
       </div>
     </div>
-    // <div className='manRope '>
-    
-
-    //     {/* Second Navbar Starts */}
-
-    //     <div className='flex justify-evenly items-center'>
-
-    //         <div className='md:flex hidden items-center'>
-    //             <input type="text" className='p-3 border-gray-200 text-gray-900 outline-none border-2 ' placeholder='Find our seach' />
-    //             <button className='p-4 border-2 text-white flex justify-center items-center text-center bg-[#333333]  hover:bg-[#F14772] text-xl'><CiSearch /></button>
-    //         </div>
-
-    //         <div className='text-2xl font-bold'>
-    //             <span
-    //                 className='font-bold'
-    //                 style={{
-    //                     background: 'linear-gradient(to right, #7957CF, #BE4E9A)',
-    //                     WebkitBackgroundClip: 'text',
-    //                     WebkitTextFillColor: 'transparent',
-    //                 }}
-    //             >
-    //                 Zen
-    //             </span>
-    //             Mart
-    //         </div>
-
-    //         <div className='flex gap-5 items-center text-2xl'>
-    //             <NavLink className='hover:text-[#F14772]'><CiSearch /></NavLink>
-    //             {/* <NavLink className='hover:text-[#F14772]'><CiUser /></NavLink> */}
-    //             <NavLink className='hover:text-[#F14772] flex items-center gap-1'><CiHeart /><span className='text-xl'>(0)</span></NavLink>
-    //             <NavLink className='hover:text-[#F14772] flex items-center gap-1'><BsBoxSeam /> <span className='text-xl'>(0)</span></NavLink>
-
-    //         </div>
-
-    //     </div>
-
-    //     {/* Second Navabr Ends */}
-
-    // </div>
+   
   );
 };
 
