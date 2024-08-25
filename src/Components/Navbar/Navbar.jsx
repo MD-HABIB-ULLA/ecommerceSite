f93101b656e7073d2f681aa9843dbafbc6a284e6
KMTonmoy <tonmoyahamed2009@gmail.com>
2024-08-25 09:18:52 +0600

    First Navbar added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import { FaMapMarkerAlt, FaUsers, FaTruck, FaInfoCircle, FaPhone, FaClipboardList } from 'react-icons/fa';
 
 const Navbar = () => {
     return (
         <div>
-            <h1>This is Navbar</h1>
+            <div className=' flex justify-evenly manRope text-[#808080] border-b-2 py-3'>
+                <div className='md:flex hidden gap-4 items-center'>
+                    <h4 className='flex items-center gap-1'><FaMapMarkerAlt /> Find Stores</h4>
+                    <h4 className='flex items-center gap-1'><FaUsers /> 100k Followers</h4>
+                </div>
+                <div className='flex items-center'>
+                    <h4 className='flex items-center gap-1'><FaTruck /> Free delivery on orders over $100 discount.</h4>
+                </div>
+                <div className='md:flex hidden'>
+                    <ul className='flex gap-4 items-center'>
+                        <li className='flex items-center gap-1'><FaInfoCircle /> About us</li>
+                        <li className='flex items-center gap-1'><FaPhone /> Contact us</li>
+                        <li className='flex items-center gap-1'><FaClipboardList /> Order tracking</li>
+                    </ul>
+                </div>
+            </div>
+
+
+            {/* Second Navbar Starts */}
+
+            
+
+            
+
+
         </div>
     );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,10 +6,10 @@ import Footer from '../../Components/Footer/Footer';
 const Home = () => {
     return (
         <div>
-            <Navbar />
+
             <Banner />
             <h1>This is Home page</h1>
-            <Footer />
+
 
         </div>
     );
